053f9e8c2aa580a3924385bdd5ed04fece020ba1
Harsh Shandilya <msfjarvis@gmail.com>
2016-12-30 23:31:42 -0600

    [2/2] jalebi : Kill IMS
    Revert "jalebi: [TEMP] IMS"
    
    This reverts commit 22e3be2875f40ad2476279e8c040a12857c8e2f0.
    
    Revert "jalebi: IMS: Update makefiles"
    
    This reverts commit 6a1fbcf74a4b901dd2ca97912be3562f05e73f10.
    
    Revert "jalebi: Add IMS blobs from Paradox"
    
    This reverts commit 3f4408daf5f30c85befb4f6adee1e289c1c582a9.
    
    Change-Id: I08b302ba153997dfea3b07343639f0f7b68b867f

--- jalebi/Android.mk
+++ jalebi/Android.mk
@@ -51,26 +51,6 @@ LOCAL_MODULE_SUFFIX := .so
 LOCAL_PROPRIETARY_MODULE := true
 include $(BUILD_PREBUILT)
 
-#include $(CLEAR_VARS)
-#LOCAL_MODULE := ims
-#LOCAL_MODULE_OWNER := yu
-#LOCAL_SRC_FILES := proprietary/vendor/app/ims/ims.apk
-#LOCAL_CERTIFICATE := platform
-#LOCAL_MODULE_TAGS := optional
-#LOCAL_MODULE_CLASS := APPS
-#LOCAL_MODULE_SUFFIX := .apk
-#include $(BUILD_PREBUILT)
-
-#include $(CLEAR_VARS)
-#LOCAL_MODULE := ims
-#LOCAL_MODULE_OWNER := yu
-#LOCAL_SRC_FILES := proprietary/vendor/app/imssettings/imssettings.apk
-#LOCAL_CERTIFICATE := platform
-#LOCAL_MODULE_TAGS := optional
-#LOCAL_MODULE_CLASS := APPS
-#LOCAL_MODULE_SUFFIX := .apk
-#include $(BUILD_PREBUILT)
-
 include $(CLEAR_VARS)
 LOCAL_MODULE := shutdownlistener
 LOCAL_MODULE_OWNER := yu
@@ -89,15 +69,6 @@ LOCAL_CERTIFICATE := platform
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_SUFFIX := .apk
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := imscmlibrary
-LOCAL_MODULE_OWNER := yu
-LOCAL_SRC_FILES := proprietary/vendor/framework/imscmlibrary.jar
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := JAVA_LIBRARIES
-LOCAL_MODULE_SUFFIX := .jar
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
